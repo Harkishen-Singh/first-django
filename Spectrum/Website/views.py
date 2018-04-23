8f48aa9e0afdbb2050c1de5f5bf3cf6546da0a55
Harkishen Singh <harkishensingh@hotmail.com>
2018-04-23 19:30:00 +0530

    next content

--- Spectrum/Website/views.py
+++ Spectrum/Website/views.py
@@ -5,4 +5,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
     
